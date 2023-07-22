4a1adf0c95ab21d44ffae7574a399771d2a415dd
hvpexe <phuhuynh923@gmail.com>
2023-07-22 07:48:12 +0700

    update https

--- src/Utils/request.js
+++ src/Utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: 'http://flaundry.somee.com/api/',
+  baseURL: 'https://flaundry.somee.com/api/',
 //   baseURL: "https://tiktok.fullstack.edu.vn/api/",
   headers: {
     "Content-Type": "application/json",
